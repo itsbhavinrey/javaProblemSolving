1c7fdbff335ceaf8d1da506a29c0b95000cf7e31
Bhavin pandya <71822008+itsbhavinrey@users.noreply.github.com>
2022-07-25 22:55:50 +0530

    Add files via upload

---
+++ isPrime.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class isPrime {
+    public static void main (String[] args){
+        Scanner scn = new Scanner(System.in);
+        int number = scn.nextInt();
+
+        //isPrime
+        int count = 0;
+        for(int i=2;i*i<=number;i++){
+            if(number%i==0){
+                count++;
+                break;
+            }
+        }
+
+        if(count == 0){
+            System.out.println("Prime");
+        }
+        else System.out.println("Not a Prime");
+    }
+}

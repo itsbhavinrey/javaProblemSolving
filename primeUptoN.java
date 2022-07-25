38c1700ad13b70c1c8d4075a56b182f127073bb6
Bhavin pandya <71822008+itsbhavinrey@users.noreply.github.com>
2022-07-25 23:09:17 +0530

    Add files via upload

---
+++ primeUptoN.java
@@ -0,0 +1,27 @@
+import java.util.*;
+ class primeUptoN {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int low = scn.nextInt();
+        int high = scn.nextInt();
+
+    //print all prime numbers till n
+    for(int i=low; i<=high; i++ ){
+        int count=0;
+
+        // check for every i that is it prime or Not
+        for(int j=2; j*j<=i; j++){
+            if(i%j==0){
+                count++;
+                break;
+            }
+        }
+        
+        if(count==0){
+            System.out.println(i);
+        }
+    }        
+
+        scn.close();
+    }
+}

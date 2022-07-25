b56ca90a3131afa42ee2d33bfcc80f355f671163
Bhavin pandya <71822008+itsbhavinrey@users.noreply.github.com>
2022-07-25 23:40:44 +0530

    Add files via upload

---
+++ 1.generalMathProblems/fibonacci.java
@@ -0,0 +1,19 @@
+import java.util.*;
+ class fibonacci {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        //this is a fibonacci where 1st term is 0 an 2nd term is 1
+        int a=0;
+        int b=1;
+        int c;
+        for(int k=1; k<=n; k++){
+            System.out.println(a);
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        scn.close();
+    }
+}
+ 

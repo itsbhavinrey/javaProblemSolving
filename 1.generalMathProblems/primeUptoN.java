aa95cab746408610289e3754e578150fbd74e097
Bhavin pandya <71822008+itsbhavinrey@users.noreply.github.com>
2022-07-26 14:25:45 +0530

    Update primeUptoN.java

--- 1.generalMathProblems/primeUptoN.java
+++ 1.generalMathProblems/primeUptoN.java
@@ -1,5 +1,5 @@
 import java.util.*;
- class primeUptoN {
+public class primeUptoN {
     public static void main(String[] args){
         Scanner scn = new Scanner(System.in);
         int low = scn.nextInt();

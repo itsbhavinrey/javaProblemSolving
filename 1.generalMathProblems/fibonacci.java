15a6343d54460326d931b5d28faf8c1cb010c3b1
Bhavin pandya <71822008+itsbhavinrey@users.noreply.github.com>
2022-07-26 14:25:58 +0530

    Update fibonacci.java

--- 1.generalMathProblems/fibonacci.java
+++ 1.generalMathProblems/fibonacci.java
@@ -1,5 +1,5 @@
 import java.util.*;
- class fibonacci {
+ public class fibonacci {
     public static void main(String[] args){
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
